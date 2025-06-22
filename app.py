f3014193961d93319deff23be1411d80592faecd
._code.__.phoenix_. <codephoenix2004@gmail.com>
2025-06-22 22:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.write("Upload a leaf image and let the AI diagnose its health status.")
 # -------------- Load your model --------------
 @st.cache_resource
 def load_plant_model():
-    model = load_model("plant_disease_model_retrained.4.keras")
+    model = load_model("plant_disease_model_retrained.3.keras")
     return model
 
 model = load_plant_model()
